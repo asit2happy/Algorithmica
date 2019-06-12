4b4e9b2610963f60649a186cd183830b7da14c0e
611329859 <611329859@HYDTC221084040.iuser.iroot.adidom.com>
2019-06-12 20:01:24 +0530

    Tree size

---
+++ 4. BinaryTree/src/SizeOfTree/BTNode.java
@@ -0,0 +1,12 @@
+package SizeOfTree;
+
+public class BTNode {
+	
+	Integer data;
+	BTNode left;
+	BTNode right;
+	public BTNode(Integer data) {
+		this.data= data;
+	}
+
+}
---
+++ 4. BinaryTree/src/SizeOfTree/TreeSize.java
@@ -0,0 +1,97 @@
+package SizeOfTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeSize {
+
+	// Tc = O(n), Sc = O(n) [In worst case maximum stack memory will be utilised for
+	// one recursion is one-sided-Binary tree of size n]
+	public static int treeSize1(BTNode root) {
+		if (root == null)
+			return 0;
+		if (root.left == null && root.right == null)
+			return 1;
+		// Reduction
+		int ls = treeSize1(root.left);
+		int rs = treeSize1(root.right);
+
+		// solution building
+		return ls + rs + 1;
+	}
+
+	// Level Order traversal [Breadth first Traversal]
+	// Tc=O(n), Sc=(n)[worst case Balanced Binary Tree where leaf = n/2 where n =
+	// total node]
+	public static int treeSize2(BTNode root) {
+		Queue<BTNode> q = new LinkedList<>();
+		q.add(root);
+		int count = 0;
+		while (!q.isEmpty()) {
+			BTNode temp = q.remove();
+			count++;
+			if (temp.left != null)
+				q.add(temp.left);
+			if (temp.right != null)
+				q.add(temp.right);
+		}
+		return count;
+	}
+
+	// Tc=n; Sc=1
+	public static int treeSize3(BTNode root) {
+		int count = 0;
+		while (root != null)
+
+			if (root.left != null) {
+				BTNode temp = inorder_predecessor(root);
+				if (temp.right == null) {
+					temp.right = root;
+					root = root.left;
+					count++;
+				} else {
+					temp.right = null;
+					root = root.right;
+				}
+			} else {
+				root = root.right;
+				count++;
+			}
+		return count;
+	}
+
+	public static int treeSize4(BTNode root) {
+		int count = 0;
+		while (root != null) {
+			if (root.left != null) {
+				BTNode temp = inorder_predecessor(root);
+				if (temp.right == null) {
+					temp.right = root;
+					root = root.left;
+					count++;
+				} else {
+					temp.right = null;
+					root = root.right;
+				}
+			} else {
+				root = root.right;
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private static BTNode inorder_predecessor(BTNode p) {
+		BTNode current = p;
+		p = p.left;
+		while (p.right != null && p.right != current) {
+			p = p.right;
+		}
+		return p;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
